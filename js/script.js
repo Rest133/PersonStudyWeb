58896bab17ee108016012ce3c1b4e9376969e8eb
Rest133 <dubrovinanatolii@mail.ru>
2020-11-11 12:49:07 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+const userList = [];
+
+userList[0] = prompt("Write login","");
+userList[1] = prompt("Write password","");
+userList[2] = prompt("Write name","");
+
+console.log(userList);
+
+
+const category = "toys";
+
+console.log(`https://someurl.com/${category}/5`);
