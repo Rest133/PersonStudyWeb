3c63db629768acaf76987866e4bb5422aa2e0985
Rest133 <dubrovinanatolii@mail.ru>
2020-11-17 17:26:34 +0300

    Fundamentals of DOM

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 "use strict";
-const userList = [];
+// const userList = [];
+//
+// userList[0] = prompt("Write login","");
+// userList[1] = prompt("Write password","");
+// userList[2] = prompt("Write name","");
+//
+// console.log(userList);
+//
+//
+// const category = "toys";
+//
+// console.log(`https://someurl.com/${category}/5`);
 
-userList[0] = prompt("Write login","");
-userList[1] = prompt("Write password","");
-userList[2] = prompt("Write name","");
+const box = document.getElementById("box");
 
-console.log(userList);
+console.log(box);
 
+const btns = document.getElementsByTagName("button");
 
-const category = "toys";
+const btn = document.getElementsByTagName("button")[2];
 
-console.log(`https://someurl.com/${category}/5`);
+const circles = document.getElementsByClassName("circle");
+
+const hearts = document.querySelectorAll('.name');
+
+const oneHeart = document.querySelector('.heart');
+
+box.style.backgroundColor = "blue";
