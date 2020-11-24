ad98dc710ffcf070eb38bd3a2a41254e4c889729
Rest133 <dubrovinanatolii@mail.ru>
2020-11-24 14:15:13 +0300

    Add XMLHttpRequest on page

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -198,4 +198,51 @@ window.addEventListener("DOMContentLoaded", () => {
         'и импортных вегетарианских стейков.',
         "430",
     ).render('.menu__field')
+
+
+    //Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро с вами свяжутся',
+        failure: 'Что-то пошло не так...'
+    }
+
+    forms.forEach(item => {
+        postData(item);
+    })
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault(); // чтобы страница не перезагружалась при отправки формы, так как это стандартное поведени браузера
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+
+            request.open('POST', 'server.php');
+            //request.setRequestHeader('Content-type', 'multipart/form-data');
+            const formData = new FormData(form); // при верстке важно указывать аттрибут name в инпутах формы
+
+            request.send(formData);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000)
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            })
+        })
+    }
 })
