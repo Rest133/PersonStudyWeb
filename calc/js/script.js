9adf9c000f2be016289db4a5ec9cd7cf83547569
Rest133 <dubrovinanatolii@mail.ru>
2020-11-24 13:31:58 +0300

    AJAX XMLHttpRequest

---
+++ calc/js/script.js
@@ -0,0 +1,27 @@
+"use strict"
+
+const inputRUB = document.querySelector('#rub'),
+    inputUSD = document.querySelector('#usd');
+
+inputRUB.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('load', () => {  //readystatechange
+        if (request.status === 200) {
+            const data = JSON.parse(request.response);
+            inputUSD.value = (+inputRUB.value / data.current.usd).toFixed(2);
+        } else {
+            inputUSD.value = 'error';
+        }
+    })
+
+    //status
+    //statusText
+    //response
+    //readyState
+})
+
